a876e0176ffa238be7a5a784987c7a8d67710e0b
Niger Hall <h.niger@yahoo.com>
2014-12-30 08:31:18 -0500

    initial commit

---
+++ app/views/contacts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @contact, :id, :name, :relationship, :phone_number, :email, :created_at, :updated_at
---
+++ db/migrate/20141230132846_create_contacts.rb
@@ -0,0 +1,12 @@
+class CreateContacts < ActiveRecord::Migration
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :relationship
+      t.integer :phone_number
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
